5d167bfe89025b774500d9c5413c2a03e18b4a12
Ilia <istudyatuni@gmail.com>
2025-05-03 19:41:21 +0300

    format facts panel component

--- frontend/src/components/FactsPanel.svelte
+++ frontend/src/components/FactsPanel.svelte
@@ -1,53 +1,53 @@
 <script>
-	let { facts = [] } = $props();
+  let { facts = [] } = $props();
 </script>
 
 <div class="facts" class:hidden={facts.length === 0}>
-	<ul>
-		{#each facts as fact}
-			<li>{fact}</li>
-		{/each}
-	</ul>
+  <ul>
+    {#each facts as fact}
+      <li>{fact}</li>
+    {/each}
+  </ul>
 </div>
 
 <style lang="scss">
-	$color: #3280ff;
+  $color: #3280ff;
 
-	.facts {
-		position: absolute;
-		bottom: 2%;
-		left: 5%;
+  .facts {
+    position: absolute;
+    bottom: 2%;
+    left: 5%;
 
-		height: auto;
-		min-height: 2em;
-		width: 90%;
+    height: auto;
+    min-height: 2em;
+    width: 90%;
 
-		border: 2px $color solid;
-		background-color: #02101b;
-		font-size: 1.3em;
+    border: 2px $color solid;
+    background-color: #02101b;
+    font-size: 1.3em;
 
-		// above map
-		z-index: 1000;
+    // above map
+    z-index: 1000;
 
-		& ul {
-			$m: 0.5em;
+    & ul {
+      $m: 0.5em;
 
-			list-style: none;
-			margin: $m auto $m -16px;
-		}
+      list-style: none;
+      margin: $m auto $m -16px;
+    }
 
-		& ul li {
-			font-family: monospace;
-		}
+    & ul li {
+      font-family: monospace;
+    }
 
-		& ul li:before {
-			content: "\2014 ";
-			color: $color;
-			margin-left: -1.5em;
-			margin-right: 1em;
-		}
-	}
-	.hidden {
-		display: none;
-	}
+    & ul li:before {
+      content: "\2014 ";
+      color: $color;
+      margin-left: -1.5em;
+      margin-right: 1em;
+    }
+  }
+  .hidden {
+    display: none;
+  }
 </style>
