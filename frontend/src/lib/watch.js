8e747a6ab1391273991543801288866718ad2ae0
Ilia <istudyatuni@gmail.com>
2025-08-09 12:12:41 +0300

    web: remove unnecessary hack for watch

--- frontend/src/lib/watch.js
+++ frontend/src/lib/watch.js
@@ -1,13 +1,7 @@
 export function listen_profile_update(id) {
-	let now = Date.now();
-
 	let url = import.meta.env.VITE_SERVER + "/api/watch?id=" + id;
 	const ev = new EventSource(url);
 	ev.onmessage = (event) => {
-		if (Date.now() - now < 3000) {
-			console.log("skipping reload because just reloaded already");
-			return;
-		}
 		if (event.data == "save-updated") {
 			console.log("got event:", event);
 			window.location.reload();
