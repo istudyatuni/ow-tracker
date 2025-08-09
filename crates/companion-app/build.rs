cd366786af098d9de52b498c1d58105ed03f35a4
Ilia <istudyatuni@gmail.com>
2025-08-09 18:00:17 +0300

    ci: check rust

--- crates/companion-app/build.rs
+++ crates/companion-app/build.rs
@@ -1,3 +1,14 @@
+use std::path::PathBuf;
+
 fn main() {
-    println!("cargo::rerun-if-changed=../../.env");
+    let envs = ["SERVER_HOST", "SERVER_PORT", "DB_PATH", "WEB_ORIGIN"];
+    if PathBuf::from("../../.env").exists() {
+        println!("cargo::rerun-if-changed=../../.env");
+    }
+    for env in envs {
+        println!("cargo::rerun-if-env-changed={env}");
+        if let Ok(value) = std::env::var(env) {
+            println!("cargo::rustc-env={env}={value}");
+        }
+    }
 }
